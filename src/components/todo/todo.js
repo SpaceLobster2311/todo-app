8de9529f03bfe7f4ef09c59cfcd76d7ebaa81847
Alexander Williams <alex.williams2311@gmail.com>
2021-07-26 20:57:23 -0700

    lab 31

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import useForm from '../../hooks/form.js';
-
+import Form from '../form.js';
+import List from '../List.js';
+import Items from '../../context/Items.js'
+import Header from '../Header.js';
 import { v4 as uuid } from 'uuid';
 
 const ToDo = () => {
 
   const [list, setList] = useState([]);
-  const [incomplete, setIncomplete] = useState([]);
+  const [incomplete, setIncomplete] = useState(0);
   const { handleChange, handleSubmit } = useForm(addItem);
 
   function addItem(item) {
@@ -37,48 +40,19 @@ const ToDo = () => {
   useEffect(() => {
     let incompleteCount = list.filter(item => !item.complete).length;
     setIncomplete(incompleteCount);
-    document.title = `To Do List: ${incomplete}`;
+    // document.title = `To Do List: ${incomplete}`;
   }, [list]);
 
+  
   return (
     <>
-      <header>
-        <h1>To Do List: {incomplete} items pending</h1>
-      </header>
+      <Items>
+        <Header data={incomplete}/>
+      <Form handleSubmit={handleSubmit} handleChange={handleChange}/>
 
-      <form onSubmit={handleSubmit}>
+      <List toggleComplete={toggleComplete} list={list}/>
+      </Items>
 
-        <h2>Add To Do Item</h2>
-
-        <label>
-          <span>To Do Item</span>
-          <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
-        </label>
-
-        <label>
-          <span>Assigned To</span>
-          <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
-        </label>
-
-        <label>
-          <span>Difficulty</span>
-          <input onChange={handleChange} defaultValue={3} type="range" min={1} max={5} name="difficulty" />
-        </label>
-
-        <label>
-          <button type="submit">Add Item</button>
-        </label>
-      </form>
-
-      {list.map(item => (
-        <div key={item.id}>
-          <p>{item.text}</p>
-          <p><small>Assigned to: {item.assignee}</small></p>
-          <p><small>Difficulty: {item.difficulty}</small></p>
-          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-          <hr />
-        </div>
-      ))}
 
     </>
   );
